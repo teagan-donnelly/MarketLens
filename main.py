7c40649d1957f5f6bb98485c5fdee59828fb0dd6
teagan-donnelly <teaganad@email.sc.edu>
2026-06-14 18:05:28 -0400

    Update Flask app pages

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template
 
+app = Flask(__name__) 
+
 @app.route('/')
 def index():
     return render_template("index.html")
@@ -10,3 +12,7 @@ def info():
  
 if __name__ == '__main__':
     app.run(debug=True)
+
+@app.route('/info', methods=['GET', 'POST'])
+def info():
+    return render_template('info.html')
