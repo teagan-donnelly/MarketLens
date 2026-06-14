29bc02e5cf41fc8d07b1ecce9ef910496fb3cb0e
teagan-donnelly <teaganad@email.sc.edu>
2026-06-14 15:32:46 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-from flask import Flask
- 
-app = Flask(__name__)
- 
+from flask import Flask, render_template
 @app.route('/')
 def index():
-    return "Hello world!"
+    return render_template("index.html")
  
 if __name__ == '__main__':
     app.run(debug=True)
